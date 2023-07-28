188308486f23c026388e092b7cfb0a537d9acee2
Devin Baeten <dbaeten@brillionsd.org>
2023-07-28 11:27:39 -0500

    Added enforcement verbiage

--- service_worker.js
+++ service_worker.js
@@ -17,4 +17,17 @@ function removeLinks() {
       link.remove();
     }
   });
+
+  const paragraphs = document.querySelectorAll('p');
+  paragraphs.forEach((p) => {
+    if (p.textContent === 'Your domain will soon enforce 2-Step Verification to ensure better account security.') {
+      p.textContent = 'Your organization requires you to use 2-Step Verification to ensure better account security';
+    } else if (p.textContent === 'To avoid being locked out of your account please enroll into 2-Step Verification now.') {
+      p.innerHTML = '<b>You must enroll into 2-Step Verification now or you will get locked out of your account.</b>';
+    } else if (p.textContent.startsWith('This policy will be enforced from')) {
+      p.remove();
+    }
+  });
 }
+
+
