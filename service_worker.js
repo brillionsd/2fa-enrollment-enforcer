2babb9498f889914a187da57c6eac23ff8223491
Devin Baeten <dbaeten@brillionsd.org>
2023-07-28 11:17:44 -0500

    Bug fixes

--- service_worker.js
+++ service_worker.js
@@ -1,16 +1,19 @@
-chrome.action.onClicked.addListener((tab) => {
-  if(tab.url.includes('https://myaccount.google.com/interstitials/twosvrequired')) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: removeLinks,
-    });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.url) {
+    const urlObj = new URL(tab.url);
+    if (urlObj.pathname.includes('/interstitials/twosvrequired')) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: removeLinks
+      }).catch(error => console.error(error));
+    }
   }
 });
 
 function removeLinks() {
   const links = document.querySelectorAll('a');
   links.forEach((link) => {
-    if (link.innerText === 'Do this later') {
+    if (link.textContent === 'Do this later') {
       link.remove();
     }
   });
